c2ce5cb5ad21728f22ebc3a6a8a36a03d6f90267
xueyingbing <xueyingbing@gmail.com>
2025-04-10 04:05:34 +0800

    feature: add call api didn't by consul demo

--- consul-consumer/src/main/java/com/ice/comsumer/client/ProviderClient.java
+++ consul-consumer/src/main/java/com/ice/comsumer/client/ProviderClient.java
@@ -5,6 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @FeignClient(name = "service-provider", configuration = FeignConfig.class)
+//@FeignClient(name = "providerClient", url = "http://localhost:8081")
 public interface ProviderClient {
     @GetMapping("/hello")  // 映射到 Provider 的接口路径
     String hello();
